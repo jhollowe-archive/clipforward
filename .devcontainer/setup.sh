4520f3ab6fb81a9e65f44f6a4c026f24a95c925e
John Hollowell <jhollowe@johnhollowell.com>
2023-09-03 16:59:55 +0000

    WIP Add cobra

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,3 +2,7 @@
 
 # install module dependencies
 go get
+
+# install cobra-cli and inject config file
+go install github.com/spf13/cobra-cli@latest
+ln -s $PWD/.devcontainer/.cobra.yaml $HOME/.cobra.yaml

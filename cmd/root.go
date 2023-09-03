860e0b8a9c385e98683578b58972a8fa68d6c0f7
John Hollowell <johnahollowell@gmail.com>
2023-09-03 20:25:51 +0000

    Add cobra command base layout

---
+++ cmd/root.go
@@ -0,0 +1,101 @@
+/*
+Copyright © 2023 John Hollowell <jhollowe@johnhollowell.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "clipforward",
+	Short: "A tool to forward TCP connections via the clipboard",
+	Long:  `Used as a pair on the destination and source, this tool uses a shared clipboard (i.e RDP, VNC, TeamViewer, AnyDesk, etc) to forward a TCP port from one host to another.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/clipforward.yaml)")
+	rootCmd.PersistentFlags().CountP("verbose", "v", "Adds verbose (and debug if specified twice) logging")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		conf_dir := path.Join(home, ".config/")
+
+		dir, is_set := os.LookupEnv("XDG_CONFIG_HOME")
+		if is_set {
+			conf_dir = dir
+		}
+
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(conf_dir)
+		viper.AddConfigPath("/config/")
+
+		// Search config in home directory with name "clipforward" (without extension).
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("clipforward")
+	}
+
+	viper.SetEnvPrefix("clip")
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}

5dec497c49d395684704f4c69dda0ce536771d23
John Hollowell <jhollowe@johnhollowell.com>
2023-09-09 14:32:08 +0000

    Only setup virtual fb in devcontainer if needed

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -2,11 +2,14 @@
 
 echo "Running docker-entrypoint.sh"
 
-# create a virtual frame buffer for X11
-nohup Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+# only create a virtual framebuffer if there is not already a DISPLAY set
+if [ -z "$DISPLAY" ]; then
+	# create a virtual frame buffer for X11
+	nohup Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
-export DISPLAY=:99.0
-echo "export DISPLAY=:99.0" > /etc/profile.d/01-export-virtual-display.sh
-chmod +x /etc/profile.d/01-export-virtual-display.sh
+	export DISPLAY=:99.0
+	echo "export DISPLAY=:99.0" > /etc/profile.d/01-export-virtual-display.sh
+	chmod +x /etc/profile.d/01-export-virtual-display.sh
+fi
 
 exec "$@"

1acc774db193bf30ebff4af00a80083c46265773
John Hollowell <johnahollowell@gmail.com>
2023-09-09 19:51:19 +0000

    Improve doPing
    this fixes the issue where if the client start before the server, it would never sync correctly

--- cmd/common.go
+++ cmd/common.go
@@ -12,21 +12,20 @@ func doPing(ctl_write chan string, ctl_read <-chan string) bool {
 	// Attempt a ping in a goroutine
 	ping_result := make(chan bool, 1)
 	go func() {
-		// ctl_write <- CLEAR
-		// time.Sleep(CLIPBOARD_INTERVAL)
+		ctl_write <- CLEAR
+		time.Sleep(CLIPBOARD_INTERVAL)
 		ctl_write <- PING
-		for msg := range ctl_read {
+
+		// wait for a response and return false if timed out
+		select {
+		case msg := <-ctl_read:
 			ping_result <- (msg == PONG)
+		case <-time.After(CLIPBOARD_INTERVAL * 3):
+			ping_result <- false
 		}
 	}()
 
-	// wait for a response and return false if timed out
-	select {
-	case success := <-ping_result:
-		return success
-	case <-time.After(CLIPBOARD_INTERVAL * 3):
-		return false
-	}
+	return <-ping_result
 }
 
 func errorAndExit(cmd *cobra.Command, exit_code int, str string) {
